2222a64e6efe2857f718f6d115a8bccfcd0bfbee
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-04 20:12:33 -0300

    arquivo criado com um array de produtos

---
+++ src/components/listaProdutos.js
@@ -0,0 +1,17 @@
+export const listaProdutos = [
+    {
+        id: 1,
+        nome: 'Teclado',
+        preco: 150
+    },
+    {
+        id: 2,
+        nome: 'Mouse',
+        preco: 120
+    },
+    {
+        id: 3,
+        nome: 'Monitor',
+        preco : 950
+    }
+]
