5279a520fc0060fe7b5f5fa3b7a76c7f780a10f0
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-04 20:18:30 -0300

    adicionado os os links para editar os produtos

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../components/listaProdutos"
+
 export default function Produtos() {
   return (
     <>
       <main>
         <h1>Produtos</h1>
+        {listaProdutos.map(prod => (
+          <div key={prod.id}>
+            <Link to={`/produtos/editar/${prod.id}`}>
+              Editar o produto: {prod.nome}
+            </Link>
+          </div>
+        ))}
       </main>
     </>
   );
