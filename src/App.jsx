17fc17b2f02381bc2efa7ee9245549cad26f680c
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-05 19:18:25 -0300

    Problema no vercel, mas já corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from "/vite.svg";
 
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Menu from "./components/menu";
-import Rodape from "./components/Rodape"
+import Menu from "./components/Menu";
+import Rodape from "./components/Rodape";
 
 function App() {
   return (
