d67bbcc0d8b878e5608aea88ecb6b7eb8b47c10d
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-06 11:12:56 -0300

    slide 16 ok

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,8 +1,25 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { listaProdutos } from "../../components/listaProdutos"
+
 export default function EditarProduto() {
+
+  const lista = listaProdutos
+  const navegacao = useNavigate()
+  const {id} =useParams()
+
+  const proc = lista.filter(prod => prod.id == id)
+  const produto = proc[0]
+
+  const salvar = ()=>{
+    alert(`Produto: ${produto.nome} editado com sucesso!`)
+    return navegacao('/produtos')
+  }
   return (
     <>
       <main>
         <h1>Editando o produto</h1>
+        <p>Editando os dados do produto: {produto.nome}</p>
+        <button onClick={salvar}>Salvar</button>
       </main>
     </>
   );
