621e4ac843bf54b4565944866ece9f77f0e1a2aa
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-09-06 10:56:01 -0300

    Apaguei imports que não estava usando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Menu from "./components/Menu";
